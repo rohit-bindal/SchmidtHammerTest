a1809b15e058b79a81d075fe95f41a7087db2e5d
Rohit Bindal <rohitkumarbindal@iipe.ac.in>
2020-02-21 17:25:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function data()
 {
   var angle=parseFloat(document.getElementById('angle').value);
   var srh=parseFloat(document.getElementById('srh').value);
+  if(!angle || !srh)
+  {
+    return;
+  }
   var x2o=(x1*srh)/100;
   var v2o2=(((0.5*k*x2o*x2o)+m*9.81*x2o*Math.cos(90-angle))*2)/m;
   var v22=(v2o2*0.5*k*x1*x1)/((0.5*k*x1*x1)+m*9.81*x1*Math.cos(90-angle));
