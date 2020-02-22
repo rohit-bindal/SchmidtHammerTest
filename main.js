381b15f6db005687bbc537d6ef9fe979a24cc1cd
Rohit Bindal <rohitkumarbindal@iipe.ac.in>
2020-02-22 19:19:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,17 @@ function data()
 {
   var angle=parseFloat(document.getElementById('angle').value);
   var srh=parseFloat(document.getElementById('srh').value);
-  if(!angle || !srh)
-  {
-    return;
-  }
   var x2o=(x1*srh)/100;
   var v2o2=(((0.5*k*x2o*x2o)+m*9.81*x2o*Math.cos(90-angle))*2)/m;
   var v22=(v2o2*0.5*k*x1*x1)/((0.5*k*x1*x1)+m*9.81*x1*Math.cos(90-angle));
   var rh=(Math.sqrt(v22/v12))*100;
   var print=document.getElementById('print');
-  print.textContent="Normalized Rebound Number: "+rh;
+  if(Number.isNaN(rh))
+  {
+    return false;
+  }
+  else 
+  {
+    print.textContent="Normalized Rebound Number: "+rh;
+  }
 }
